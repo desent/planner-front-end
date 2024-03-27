ef40d079b6d87ba1b38aff39f34d643f11ba626c
Dmytro Usenko <dmytro.usenko@acoustic.co>
2024-03-27 21:39:29 +0200

    Initial config

---
+++ src/constants/color.constants.ts
@@ -0,0 +1,7 @@
+export const COLORS = {
+  PRIMARY: '#7551FF',
+  SECONDARY: '#F97912',
+  BORDER: 'rgba(255,255,255,.12)',
+  SIDEBAR: '#1415115',
+  BG: '#0E0F0F'
+}
---
+++ src/constants/seo.constants.ts
@@ -0,0 +1,3 @@
+export const NO_IND_PAGE = { robots: { index: false, follow: false }};
+export const SITE_NAME = 'Planner';
+export const SITE_DESCRIPTION = 'Site planner';
